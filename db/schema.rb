80d39b8cbb046c8db3f7b739ecc3da8ad955f32c
Dominika <d.k.krzysztofik@gmail.com>
2017-01-18 20:53:26 +0100

    +authorization +logi in views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114155602) do
+ActiveRecord::Schema.define(version: 20170118181521) do
 
   create_table "events", force: :cascade do |t|
     t.string   "artist"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 20170114155602) do
     t.string   "phone"
     t.integer  "event_id"
     t.boolean  "for_adults"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "role"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
